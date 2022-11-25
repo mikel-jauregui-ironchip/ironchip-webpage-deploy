09bfa590c6419a70cb1502a1ac3fb0712442035a
mikel.jauregui <mikel.jauregui@ironchip.com>
2022-11-25 12:22:49 +0100

    deploy

---
+++ component---src-pages-index-js-e2f474147031bdb60874.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[678],{6558:function(e,t,a){a.r(t);var l=a(7294),n=a(5592),r=a(7782);t.default=()=>{const{t:e}=(0,r.useTranslation)();return l.createElement(n.Z,null,l.createElement("h1",null,e("home")))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-e2f474147031bdb60874.js.map
---
+++ component---src-pages-page-2-js-3846111be7ede5aac76e.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[617],{4948:function(e,t,a){a.r(t);var l=a(7294),n=a(5592);t.default=()=>l.createElement(n.Z,null,l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"))}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-3846111be7ede5aac76e.js.map
